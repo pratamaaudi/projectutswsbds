221431f244ff1b43e0bb12272cabf3de1fc68ec8
pratamaaudi <pratamaaudi@gmail.com>
2017-09-13 15:51:11 +0700

    UPDATE 1
    - jalanbesar.geojson
    - koneksi database
    - load layer berdasarkan database

--- map.php
+++ map.php
@@ -1,50 +1,90 @@
 <html lang="en">
-  <head>
-    <link rel="stylesheet" href="../openlayers4/css/ol.css" type="text/css">
-    <script src="../openlayers4/build/ol.js" type="text/javascript"></script>    
-     <title>Project WSBD Gan</title>
-  </head>
-  <body>
-    <h2>My Map</h2>
-    <div id="map" class="map"></div>
-    <script type="text/javascript">
+    <head>
+        <link rel="stylesheet" href="../openlayers4/css/ol.css" type="text/css">
+        <script src="../openlayers4/build/ol.js" type="text/javascript"></script>    
+        <title>Project WSBD Gan</title>
+        <?php
+        include_once ("koneksi.php");
+        ?>
 
-        var sma = new ol.layer.Vector({
-                source: new ol.source.Vector({
+    </head>
+    <body>
+        <h2>My Map</h2>
+        <div id="map" class="map"></div>
+        <script type="text/javascript">
+
+<?php
+$sql = "SELECT * FROM layer";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        ?>
+
+                    var layer<?php echo $row['id']; ?> = new ol.layer.Vector({
+                    source: new ol.source.Vector({
                     format: new ol.format.GeoJSON({
-                        defaultDataProjection: 'EPSG:4326'
+                    defaultDataProjection: 'EPSG:4326'
                     }),
-                    url: 'sma.geojson'
-                }),
+                            url: '<?php echo $row['layer']; ?>'
+                    }),
+                    });
+        <?php
+    }
+}
+?>
+
+
+
+            var map = new ol.Map({
+            target: 'map',
+                    layers: [
+                            new ol.layer.Tile({
+                            source: new ol.source.OSM()
+                            })
+<?php
+$sql = "SELECT * FROM layer";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        ?>
+
+                            ,layer<?php echo $row['id']; ?>
+
+        <?php
+    }
+}
+?>
+                    ],
+                    controls: [
+                            //Define the default controls
+                            new ol.control.Zoom(),
+                            new ol.control.Rotate(),
+                            new ol.control.Attribution(),
+                            //Define some new controls
+                            new ol.control.ZoomSlider(),
+                            new ol.control.MousePosition(),
+                            new ol.control.ScaleLine(),
+                            new ol.control.OverviewMap()
+                    ],
+                    view: new ol.View({
+<?php
+$sql = "SELECT * FROM setting where id = 1";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        ?>
+
+                            center: ol.proj.fromLonLat([<?php echo $row['x']; ?>, <?php echo $row['y']; ?>]),
+                                    zoom: <?php echo $row['zoom']; ?>
+                            })
+
+        <?php
+    }
+}
+?>
+
             });
+        </script>
 
-
-        var map = new ol.Map({
-        target: 'map',
-        layers: [
-          new ol.layer.Tile({
-            source: new ol.source.OSM()
-          }), sma
-        ],
-
-        controls: [
-//Define the default controls
-                    new ol.control.Zoom(),
-                    new ol.control.Rotate(),
-                    new ol.control.Attribution(),
-//Define some new controls
-                    new ol.control.ZoomSlider(),
-                    new ol.control.MousePosition(),
-                    new ol.control.ScaleLine(),
-                    new ol.control.OverviewMap()
-                ],
-
-        view: new ol.View({
-          center: ol.proj.fromLonLat([112.752087, -7.257495]),
-          zoom: 13
-        })
-      });
-    </script>
-    
-  </body>
+    </body>
 </html>
