45ee1b9fb9b11bed6b0b02e3f56a83625afafd45
pratamaaudi <pratamaaudi@gmail.com>
2017-09-13 15:53:16 +0700

    UPDATE 2
    - upload database untuk update 1

---
+++ wsbdsuts.sql
@@ -0,0 +1,127 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 13, 2017 at 10:52 AM
+-- Server version: 10.1.10-MariaDB
+-- PHP Version: 7.0.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `wsbdsuts`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `layer`
+--
+
+CREATE TABLE `layer` (
+  `id` int(11) NOT NULL,
+  `warna` varchar(10) DEFAULT NULL,
+  `alpha` double DEFAULT NULL,
+  `layer` varchar(100) DEFAULT NULL,
+  `urutan` int(11) DEFAULT NULL,
+  `profile_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `layer`
+--
+
+INSERT INTO `layer` (`id`, `warna`, `alpha`, `layer`, `urutan`, `profile_id`) VALUES
+(1, NULL, NULL, 'sma.geojson', 1, 1),
+(2, NULL, NULL, 'jalanbesar.geojson', 2, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `profile`
+--
+
+CREATE TABLE `profile` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(100) DEFAULT NULL,
+  `setting_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `profile`
+--
+
+INSERT INTO `profile` (`id`, `nama`, `setting_id`) VALUES
+(1, 'default', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `setting`
+--
+
+CREATE TABLE `setting` (
+  `id` int(11) NOT NULL,
+  `x` varchar(20) DEFAULT NULL,
+  `y` varchar(20) DEFAULT NULL,
+  `zoom` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `setting`
+--
+
+INSERT INTO `setting` (`id`, `x`, `y`, `zoom`) VALUES
+(1, '112.752087', '-7.257495', 13);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `layer`
+--
+ALTER TABLE `layer`
+  ADD PRIMARY KEY (`id`,`profile_id`),
+  ADD KEY `fk_setting_profile_idx` (`profile_id`);
+
+--
+-- Indexes for table `profile`
+--
+ALTER TABLE `profile`
+  ADD PRIMARY KEY (`id`,`setting_id`),
+  ADD KEY `fk_profile_setting1_idx` (`setting_id`);
+
+--
+-- Indexes for table `setting`
+--
+ALTER TABLE `setting`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `layer`
+--
+ALTER TABLE `layer`
+  ADD CONSTRAINT `fk_setting_profile` FOREIGN KEY (`profile_id`) REFERENCES `profile` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Constraints for table `profile`
+--
+ALTER TABLE `profile`
+  ADD CONSTRAINT `fk_profile_setting1` FOREIGN KEY (`setting_id`) REFERENCES `setting` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
